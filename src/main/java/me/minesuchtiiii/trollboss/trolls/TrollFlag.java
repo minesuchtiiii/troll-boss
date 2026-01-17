1be93aa18f6b829d34dd1b200fde8b20ecba2d88
Diego <diego060900@gmail.com>
2026-01-17 10:06:42 +0100

    Fix typo in `PREVENT_GROUND_MOVEMENT` comment: Correct axes from `(x, y)` to `(x, z)`

--- src/main/java/me/minesuchtiiii/trollboss/trolls/TrollFlag.java
+++ src/main/java/me/minesuchtiiii/trollboss/trolls/TrollFlag.java
@@ -5,7 +5,7 @@ public enum TrollFlag {
     // Prevents movement in any direction (x, y, z)
     PREVENT_ALL_MOVEMENT,
 
-    // Prevents movement on ground (x, y), player can still change y-level
+    // Prevents movement on the ground (x, z), player can still change y-level
     PREVENT_GROUND_MOVEMENT,
 
     // PRevents player from breaking blocks
